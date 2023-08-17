c277498b373f1d9f108c94f91d184f5523b6752e
vitaolv <vlveleda@gmail.com>
2023-08-17 19:31:04 -0300

    Implementação de resultados encontrados de busca

--- src/pages/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useProductContext } from "../context/ProductContext";
 import { Link } from "react-router-dom";
+import { getFormattedPriceValue } from "../utils/prices/priceUtils";
 import { ButtonsToHomeOrToBack } from "../components/Buttons/ButtonsToHomeOrToBack";
 
 export default function SearchResultsPage() {
@@ -14,14 +15,46 @@ export default function SearchResultsPage() {
   return (
     <div className="search-results-container">
       {filteredProducts.length > 0 ? (
-        filteredProducts.map((product) => (
-          <div key={product.id} className="search-result-item">
-            <div className="search-title">
-              <p>Você digitou {term}</p>
-            </div>
-            <div className="search-results-list">{product.name}</div>
+        <div>
+          <div className="search-title">
+            <h3>Você está buscando: {term}</h3>
+            <h6>
+              Encontramos {filteredProducts.length}{" "}
+              {filteredProducts.length === 1 ? "resultado" : "resultados"}.
+            </h6>
           </div>
-        ))
+
+          <div className="search-results-list">
+            {filteredProducts.map((product) => (
+              <Link
+                key={product.objID}
+                to={`/produto/${encodeURIComponent(product.name)}/${
+                  product.id
+                }`}
+                className={"card-in-searchResults"}
+              >
+                <li key={product.id} className="search-result-item">
+                  <img
+                    src={product.cover[0]}
+                    alt={product.name}
+                    className="product-image-in-search"
+                  />
+                  <span id="idSearch">ID de item: {product.id}</span>
+                  <span>
+                    <strong>{product.name}</strong>
+                    <br />
+                  </span>
+                  <span id="item-price">
+                    <strong>
+                      Preço: <br />
+                      {getFormattedPriceValue(product.price)}
+                    </strong>
+                  </span>
+                </li>
+              </Link>
+            ))}
+          </div>
+        </div>
       ) : (
         <div className="not-found-in-search-result">
           <img src="/assets/not-found-search.png" alt="Não encontrado" />
