069e829bb1792b25eac74ef4b0bd963997bfcc13
vitaolv <vlveleda@gmail.com>
2023-08-21 17:15:16 -0300

    Mudança na seção "sobre nós" da página Home.

---
+++ src/components/Texts/TextAboutHome.jsx
@@ -0,0 +1,25 @@
+export function TextAboutHome() {
+  return (
+    <div>
+      <div className="text-about">
+        <h2 className="text-overlay">Sobre nós</h2>
+        <div className="text-p-about">
+          <hr />
+          <p>
+            Somos uma empresa alimentícia. É uma marca de chocolate nacional,
+            iniciada em 2023 por confeiteiros. A confeitaria é uma arte nobre
+            que faz com que nossa vida fique muito mais saborosa. Afinal de
+            contas, um doce nunca é demais, não importa a hora ou a ocasião. Se
+            você é apaixonado pela doçura da vida, vai se encantar com nossos
+            produtos bem saborosos.
+          </p>
+          <p>
+            Nós, da ToSweeten, estamos fazendo a nossa parte na defesa do meio
+            ambiente! Além disso, valorizamos a diversidade cultural e humana
+            como princípios fundamentais em nossa atuação.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
