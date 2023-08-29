5aed9ded5122ba6b4bc802ab7e18f7d8452ed11c
Vitão <84293496+vitaolv@users.noreply.github.com>
2023-08-29 02:15:23 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-modal";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import StoreProvider from "./store/index";
+import StoreProvider from "./store/Index";
 
 Modal.setAppElement("#root"); // Definir o elemento de aplicativo
 
