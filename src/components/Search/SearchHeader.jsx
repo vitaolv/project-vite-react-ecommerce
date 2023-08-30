8eb391f41d973578d6f9c78b5fe9e30a5fd5cb88
Vitão <84293496+vitaolv@users.noreply.github.com>
2023-08-30 02:42:27 -0300

    Mudança no Link.

--- src/components/Search/SearchHeader.jsx
+++ src/components/Search/SearchHeader.jsx
@@ -1,40 +1,72 @@
-import { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useProductContext } from "../context/ProductContext";
+import { Link } from "react-router-dom";
+import { getFormattedPriceValue } from "../utils/prices/priceUtils";
+import { ButtonsToHomeOrToBack } from "../components/Buttons/ButtonsToHomeOrToBack";
 
-export function SearchHeader() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigate = useNavigate();
+export default function SearchResultsPage() {
+  const { term } = useParams();
+  const products = useProductContext();
 
-  const handleSearchChange = (e) => {
-    const searchTerm = e.target.value;
-    setSearchTerm(searchTerm);
-  };
-
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/project-vite-react-ecommerce/resultado-da-busca/${searchTerm}`);
-  };
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(term.toLowerCase())
+  );
 
   return (
-    <>
-      <Form className="d-flex" onSubmit={handleSearchSubmit}>
-        <Form.Control
-          type="search"
-          placeholder="Encontre aqui..."
-          className="me-2"
-          aria-label="Buscar"
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
-        <Button
-          variant="warning"
-          className="custom-search-button"
-          type="submit"
-        >
-          Buscar
-        </Button>
-      </Form>
-    </>
+    <div className="search-results-container">
+      {filteredProducts.length > 0 ? (
+        <div>
+          <div className="search-title">
+            <h3>Você está buscando: {term}</h3>
+            <h6>
+              Encontramos {filteredProducts.length}{" "}
+              {filteredProducts.length === 1 ? "resultado" : "resultados"}.
+            </h6>
+          </div>
+
+          <div className="search-results-list">
+            {filteredProducts.map((product) => (
+              <Link
+                key={product.objID}
+                to={`/project-vite-react-ecommerce/produto-${encodeURIComponent(
+                  product.name
+                )}-${product.id}`}
+                className={"card-in-searchResults"}
+              >
+                <li key={product.id} className="search-result-item">
+                  <img
+                    src={product.cover[0]}
+                    alt={product.name}
+                    className="product-image-in-search"
+                  />
+                  <span id="idSearch">ID de item: {product.id}</span>
+                  <span>
+                    <strong>{product.name}</strong>
+                    <br />
+                  </span>
+                  <span id="item-price">
+                    <strong>
+                      Preço: <br />
+                      {getFormattedPriceValue(product.price)}
+                    </strong>
+                  </span>
+                </li>
+              </Link>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="not-found-in-search-result">
+          <img src="assets/not-found-search.png" alt="Não encontrado" />
+          <h3>Não encontramos o produto que você digitou: {term}</h3>
+          <p>
+            Verifique se a palavra foi digitada corretamente ou tente novamente
+            com outro termo para busca.{" "}
+          </p>
+
+          <ButtonsToHomeOrToBack />
+        </div>
+      )}
+    </div>
   );
 }
