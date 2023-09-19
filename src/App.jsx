f23cd3cb8e1cd9934691508dc55c002e11116229
vitaolv <vlveleda@gmail.com>
2023-09-19 16:48:13 -0300

    Corrigindo caminho das rotas.

--- src/App.jsx
+++ src/App.jsx
@@ -34,16 +34,18 @@ function App() {
         <Header />
         <ProductProvider>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/produto/:nome/:id" element={<ProductDetail />} />
-            <Route path="/carrinho-e-pagamento" element={<CartWithPayment />} />
+            <Route path="/project-vite-react-ecommerce/" element={<Home />} />
             <Route
-              path="/resultado-da-busca/:term"
-              element={<SearchResultsPage />}
+              path="/project-vite-react-ecommerce/produto/:nome/:id"
+              element={<ProductDetail />}
             />
-            <Route path="*" element={<PageNotFound />} />
             <Route
-              path="/resultado-da-busca/:term"
+              path="/project-vite-react-ecommerce/carrinho-e-pagamento"
+              element={<CartWithPayment />}
+            />
+            <Route path="/*" element={<PageNotFound />} />
+            <Route
+              path="/project-vite-react-ecommerce/resultado-da-busca/:term"
               element={<SearchResultsPage />}
             />
           </Routes>
